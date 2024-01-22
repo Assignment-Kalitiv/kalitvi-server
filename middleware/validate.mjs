a5b894679b5d80f645fade800fbb7e941875a65d
Art96em <artem-w0lf@yandex.ru>
2024-01-22 09:02:58 +0200

    refactor

--- middleware/validate.mjs
+++ middleware/validate.mjs
@@ -1,11 +1,9 @@
 import { schema } from '../config/schema.mjs'
 import { throwError } from '../utils/util.js';
 
-
 const validate = (req, res, next) => {
     if (schema && req.body) {
         const { error } = schema.validate(req.body);
-        console.log(error);
         if (error) {
             throwError(res, 402, error.details[0].message)
         }
